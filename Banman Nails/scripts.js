059b566d7d3cf70362bb8c0b481672e67c1cd8ec
Palesa <palesa.mapeka@umuzi.org>
2025-09-02 20:43:18 +0200

    Added the Google Calendar feature

--- Banman Nails/scripts.js
+++ Banman Nails/scripts.js
@@ -1,4 +1,4 @@
-function HamburgerMenu() {
+function hamburgerMenu() {
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
 
@@ -7,10 +7,16 @@ function HamburgerMenu() {
   });
 }
 
-function HandleBookings() {
-  
+function handleBookings() {
+  window.addEventListener("load", function () {
+    calendar.schedulingButton.load({
+      url: "https://calendar.google.com/calendar/appointments/schedules/AcZssZ36ZyNn6xPd_y-eTmTPLE_1jOwf-9rhnhSEyBJREOqI9jYm-jyy6K1rIfdDgJZnOpj8g02NtnwT?gv=true",
+      color: "#A3B18A",
+      label: "Book an appointment",
+      target: document.getElementById("google-calendar-booking"),
+    });
+  });
 }
 
-HandleBookings();
-
-HamburgerMenu();
+handleBookings();
+hamburgerMenu();
